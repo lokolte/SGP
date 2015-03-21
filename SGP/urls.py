097d033b1f1ed561cb467222122d37a07f6f9cc5
santiago <stortora@softwarenatura.com>
2015-03-21 08:33:17 -0300

    Database configured. Remember to install psycopg2

--- SGP/urls.py
+++ SGP/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'SGP.views.home', name='home'),
+    url(r'^$', 'SGPv1.views.login', name='login'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
