a331da9bd6d4b19ed2970f0d52bfaedcba3c145f
santiago <stortora@softwarenatura.com>
2015-05-11 08:23:21 -0400

    Corregido campo telefono en UsuarioSerializer Agregue un test para testear los tests.

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -2,3 +2,14 @@
 
 ./node_modules/bower/bin/bower install
 ./node_modules/gulp/bin/gulp.js
+
+#Configurar e instalar apache
+apt-get install apache2
+apt-get install libapache2-mod-wsgi
+
+mv ./000-default.conf /etc/apache2/sites-available/000-default.conf
+service apache2 restart
+
+#Generar documentacion
+apt-get install epydoc
+epydoc SGP authentication flujos proyectos
