35b35996264047b6a20c946a673522ef0805a25c
santiago <stortora@softwarenatura.com>
2015-05-19 08:00:30 -0400

    install scripts

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,13 +3,6 @@
 ./node_modules/bower/bin/bower install
 ./node_modules/gulp/bin/gulp.js
 
-#Configurar e instalar apache
-apt-get install apache2
-apt-get install libapache2-mod-wsgi
-
-mv ./000-default.conf /etc/apache2/sites-available/000-default.conf
-service apache2 restart
-
 #Generar documentacion
 apt-get install epydoc
-epydoc SGP authentication flujos proyectos
+epydoc SGP authentication flujos proyectos sprints userstories
